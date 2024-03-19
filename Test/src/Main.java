3423e2ff88e64bd4b87767c13b4460b0031b5995
Dimitri Bolotnikov <148213666+DitoGeorgia@users.noreply.github.com>
2024-03-19 23:55:59 +0400

    add

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         double value = 5.789456;
-        System.out.printf("%.0f", value);
+        System.out.printf("%.1f", value);
     }
 }
