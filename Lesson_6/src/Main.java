8f1a0d4251dcb12751f4a7f7dd90c91e4ef582ce
DitoGeorgia <ditogeorgia@gmail.com>
2024-04-02 05:03:17 +0400

    add

---
+++ Lesson_6/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите число:" );
+        int a = new Scanner(System.in).nextInt();
+        if (a == 5) {
+            System.out.println("5");
+        }
+        a = 6;
+        if (a == 6) {
+            System.out.println("6");
+        }
+        if (a == 7 ) {
+            System.out.println("7");
+        }
+    }
+}
